106a34ff167dc277626a248a53d231f8672b55d3
do7be <bomsap700@gmail.com>
2016-12-07 22:18:59 +0900

    React化

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 const sugarss      = require('sugarss')
 const postcss      = require('postcss')
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-function runPostcss () {
-  const css = document.getElementById('src').value
+class DemoArea extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: this.props.text,
+      output: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  postcss().process(css, { parser: sugarss }).then(function (result) {
-    result.warnings().forEach(function (warn) {
-        console.warn(warn.toString())
+  componentDidMount() {
+  }
+
+  componentWillMount() {
+  }
+
+  handleChange(event) {
+    const css = event.target.value
+    this.setState({text: css})
+    postcss().process(css, { parser: sugarss }).then((result) => {
+      return new Promise((resolve, reject) => {
+        result.warnings().forEach((warn) => {
+          console.warn(warn.toString())
+        })
+        resolve(result.css)
+      })
+    }).then((css) => {
+      this.setState({output: css})
     })
-    document.getElementById('dist').textContent = result.css
-  })
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea onChange={this.handleChange} value={this.state.text} />
+        <pre><code>{this.state.output}</code></pre>
+      </div>
+    )
+  }
 }
 
-document.getElementById('src').addEventListener('keyup', runPostcss)
-runPostcss()
+var initialText = (function () {/*
+// Let's edit SugarSS
+.foo .bar
+  font-size: normal
+  display: flex
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
+ReactDOM.render(
+  <DemoArea text={initialText} />,
+  document.getElementById('demo-area')
+);
