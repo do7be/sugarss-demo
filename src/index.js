044e3b95d8524e9ce1da0568e9ba271de17e67a4
do7be <bomsap700@gmail.com>
2016-12-07 20:59:06 +0900

    SugarSS変換に成功

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-const autoprefixer = require('autoprefixer')
+const sugarss      = require('sugarss')
 const postcss      = require('postcss')
 
 function runPostcss () {
   const css = document.getElementById('src').textContent
 
-  postcss([ autoprefixer ]).process(css).then(function (result) {
-    console.log(css)
+  postcss().process(css, { parser: sugarss }).then(function (result) {
     result.warnings().forEach(function (warn) {
         console.warn(warn.toString())
     })
-    console.log(result.css)
     document.getElementById('dist').textContent = result.css
   })
 }
