95b610f30e7bcad1f6cd780b8958f1e27f28c50b
do7be <bomsap700@gmail.com>
2016-12-08 20:48:21 +0900

    babel-preset-latestを用いるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel",
         query:{
-          presets: ['react', 'es2015']
+          presets: ['react', 'latest']
         }
       },
       {
